f713fa80f70b27819565f4953ae6a66b18d7fbc1
Alfonso <benbanda07@gmail.com>
2019-02-17 23:18:18 -0600

    Init Project

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Andres', 'Guzman', 'profesor@bolsadeideas.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Alfonso', 'Banda', 'poncho@rhynyx.com', '2019-01-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Kenneth', 'Martinez', 'kenneth@kinedu.com', '2018-01-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Juan', 'Ortiz', 'juanjo@emprove.com', '2018-01-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Mariana', 'Treviño', 'mariana@oi.com', '2018-01-17');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUEs('Valeria', 'Valades', 'val.val@nexta.com', '2018-01-17');
